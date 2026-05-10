fe33b92a6f11becaaed263cb8c60b2aa3479901a
Naveen Rana <yourgithubemail@gmail.com>
2026-05-10 23:47:50 +0530

    update db

--- database.py
+++ database.py
@@ -1,16 +1,13 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+DATABASE_URL="postgresql://postgres:ranaji@localhost:2004/expense_trac"
+engine=create_engine(DATABASE_URL)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
+SessionalLocal=sessionmaker(
     autoflush=False,
+    autocommit=False,
     bind=engine
 )
+
+Base=declarative_base()
