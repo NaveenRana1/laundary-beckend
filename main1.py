765a96c9677ea3759e158be8984ec92f379b4109
Naveen Rana <yourgithubemail@gmail.com>
2026-05-11 11:01:33 +0530

    frontend url

--- main1.py
+++ main1.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["https://laundary-ui-yeyg.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
