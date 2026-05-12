6ad07d81879209d367900a8bd09d55cf3658fb84
Naveen Rana <yourgithubemail@gmail.com>
2026-05-12 15:14:00 +0530

    url setup

--- main1.py
+++ main1.py
@@ -37,7 +37,7 @@ def home():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://laundary-ui-yeyg.vercel.app"],
+    allow_origins=["https://laundary-ui-yeyg.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
